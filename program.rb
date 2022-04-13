e4542988a13cb1194d23507b1c3aca2a72c45b56
OnyJ <julien.josselin75@hotmail.fr>
2022-04-13 15:51:34 +0200

    you louse

--- program.rb
+++ program.rb
@@ -1,11 +1,23 @@
 def create_random_number
-
+  rand(0..4)
 end
 
 def get_player_guess
-
+  guess = gets.chomp
 end
 
-def is_number_valid?
+def is_number_valid
+  number = create_random_number.to_s
+  guess = get_player_guess.to_s
 
+  puts "number : " + number
+  puts "guess  : " + guess
+
+  if number == guess
+    puts "youhouy"
+  else
+    puts "you lose"
+  end
 end
+
+is_number_valid
